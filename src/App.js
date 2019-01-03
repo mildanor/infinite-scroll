5edb05284d5b82aae05e0f0e15638b0a11c2ab5c
milda norkute <menulio.kiskis@gmail.com>
2019-01-03 13:31:34 +0200

    exmaple one

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import InfiniteImage from './components/InfiniteImage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <InfiniteImage/>
       </div>
     );
   }
